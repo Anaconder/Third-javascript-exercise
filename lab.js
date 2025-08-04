6b4774ce8959d5318e4b23f2e167f4ed178df518
Kaladin Stormblessed <adedamoladediran@gmail.com>
2025-08-04 04:20:14 -0400

    sixth commit

--- lab.js
+++ lab.js
@@ -50,3 +50,49 @@ while(num1<=100){
     diff_maker++
 
   }
+
+
+// Part 3: Feeling Loopy
+let data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+let currentCell = 1;
+let i = 0;
+
+while (i < data.length) {
+  let char = data[i];
+
+  if (char === ',') {
+    currentCell += 1; // Move to next cell
+  } else if (char === '\n') {
+    // End of row: print and reset everything
+    console.log(cell1, cell2, cell3, cell4);
+    cell1 = "";
+    cell2 = "";
+    cell3 = "";
+    cell4 = "";
+    currentCell = 1;
+  } else {
+    // Add character to the correct cell
+    if (currentCell === 1) {
+      cell1 += char;
+    } else if (currentCell === 2) {
+      cell2 += char;
+    } else if (currentCell === 3) {
+      cell3 += char;
+    } else if (currentCell === 4) {
+      cell4 += char;
+    }
+  }
+
+  i += 1;
+}
+
+// After loop ends, print last row (in case it didn't end with \n)
+if (cell1 || cell2 || cell3 || cell4) {
+  console.log(cell1, cell2, cell3, cell4);
+}
